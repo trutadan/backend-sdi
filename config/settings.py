73a3c54b29a97429a7038f4e905292f155c2fabe
trutadan <trutadanalexandru@gmail.com>
2023-05-14 20:04:50 +0300

    commit 10.1.6
    managed to almost finish dockerizing...

--- config/settings.py
+++ config/settings.py
@@ -145,8 +145,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 
 # Default primary key field type
